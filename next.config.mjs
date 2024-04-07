03516f72ca1c11df76586836aec1fe3d1411d280
Daren Shamoun <darenshamoun@gmail.com>
2024-04-07 16:08:00 -0700

    attempting to get next.js to work well with github pages static generation

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,7 @@ const nextConfig = {
     reactStrictMode: true,
     basePath: isProd ? '/computer-part-extravaganza' : '', // this is the base path of the app to be deployed on GitHub Pages
     assetPrefix: isProd ? '/computer-part-extravaganza/' : '', // this is to prefix the assets with the base path for GitHub Pages
+    output: 'export' // this is to export the app as static files
 };
 
 export default nextConfig;
