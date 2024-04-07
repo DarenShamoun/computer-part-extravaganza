a2c6ebdebb7cef1a20078ce6ec87ea1eded47fb4
Daren Shamoun <darenshamoun@gmail.com>
2024-04-07 13:03:56 -0700

    changed the next config files so that it works with GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
+const isProd = process.env.NODE_ENV === 'production'; // if true, we are in production mode, which is GitHub Pages
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    reactStrictMode: true,
+    basePath: isProd ? '/computer-part-extravaganza' : '', // this is the base path of the app to be deployed on GitHub Pages
+    assetPrefix: isProd ? '/computer-part-extravaganza/' : '', // this is to prefix the assets with the base path for GitHub Pages
+};
 
 export default nextConfig;
