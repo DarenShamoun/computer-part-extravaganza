13bd2cd88c1c93c3d6e8df34219bb48e9e4a87b1
Daren Shamoun <darenshamoun@gmail.com>
2024-04-07 18:03:57 -0700

    Refactor project to use VUE.js as next.js doesnt work well with github pages

--- next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: 'export', // this is to export the app as static files
-    reactStrictMode: true,
-    basePath: '/computer-part-extravaganza', // this is the base path of the app to be deployed on GitHub Pages
-    assetPrefix: '/computer-part-extravaganza/', // this is to prefix the assets with the base path for GitHub Pages
-};
-
-export default nextConfig;
