62629bcdc0ae00dd75f20138ea20abcf4991bf99
Daren Shamoun <darenshamoun@gmail.com>
2024-04-07 19:07:13 -0700

    Update main.js to import and use Vue Bootstrap for component styling

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue-next/dist/bootstrap-vue-next.css'; 
+import { createBootstrap } from 'bootstrap-vue-next';
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.use(createBootstrap());
+app.mount('#app');
