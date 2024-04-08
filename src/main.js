3443d2a3ebe64d7f5ce9cebe2cf3c1016b0283f1
Daren Shamoun <darenshamoun@gmail.com>
2024-04-07 20:47:59 -0700

    Update Vuetify theme to enable dark mode and customize colors

--- src/main.js
+++ src/main.js
@@ -4,12 +4,30 @@ import App from './App.vue';
 // Vuetify
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
+import * as colors from 'vuetify/lib/util/colors';
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 
 const vuetify = createVuetify({
   components,
   directives,
+  theme: {
+    defaultTheme: 'dark', // Force dark mode
+    themes: {
+      dark: {
+        dark: true, // Enable dark mode
+        colors: {
+          primary: colors.blue.darken2, // Darkened blue for primary
+          secondary: colors.amber.darken3, // Darkened amber for secondary
+          accent: colors.shades.black, // Black for accents
+          error: colors.red.accent3, // Red for error messages
+          info: colors.blue.lighten1, // Light blue for info messages
+          success: colors.green.accent3, // Green for success states
+          warning: colors.amber.base, // Amber for warnings
+        },
+      },
+    },
+  },
 })
 
 createApp(App).use(vuetify).mount('#app')
