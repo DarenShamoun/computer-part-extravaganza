0e509523dc0e63d18f88c550b4a892d8226d5734
Daren Shamoun <darenshamoun@gmail.com>
2024-04-07 20:17:23 -0700

    Switched to Vuetify instead of bootstrap Vue for compatability issue

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,9 @@
-<script setup>
-</script>
-
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/favicon.ico" class="logo" alt="Vite logo" />
-    </a>
+    <Navbar />
   </div>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<script setup>
+  import Navbar from './components/Navbar.vue';
+</script>
