a2e0f513b36b850d61956d6d46ec699461f7c421
Daren Shamoun <darenshamoun@gmail.com>
2024-04-07 19:09:45 -0700

    Remove reference to deleted Vue logo file and starting from a blank slate

--- src/App.vue
+++ src/App.vue
@@ -6,9 +6,6 @@
     <a href="https://vitejs.dev" target="_blank">
       <img src="/favicon.ico" class="logo" alt="Vite logo" />
     </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
   </div>
 </template>
 
