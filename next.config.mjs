a9d7373e59c4732b11e20f1c93743101989140f1
Daren Shamoun <darenshamoun@gmail.com>
2024-04-07 17:01:27 -0700

    Refactor GitHub Actions workflows and Next.js configuration for GitHub Pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,9 @@
-const isProd = process.env.NODE_ENV === 'production'; // if true, we are in production mode, which is GitHub Pages
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: 'export', // this is to export the app as static files
     reactStrictMode: true,
-    basePath: isProd ? '/computer-part-extravaganza' : '', // this is the base path of the app to be deployed on GitHub Pages
-    assetPrefix: isProd ? '/computer-part-extravaganza/' : '', // this is to prefix the assets with the base path for GitHub Pages
-    output: 'export' // this is to export the app as static files
+    basePath: '/computer-part-extravaganza', // this is the base path of the app to be deployed on GitHub Pages
+    assetPrefix: '/computer-part-extravaganza/', // this is to prefix the assets with the base path for GitHub Pages
 };
 
 export default nextConfig;
